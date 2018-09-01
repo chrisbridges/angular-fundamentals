d4262fc6c820646698ed114d76b6cad1c355b780
chris.bridges <christopher_bridges@icloud.com>
2018-09-01 13:40:58 -0400

    one-way data binding example

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,15 @@ import { Component } from '@angular/core'
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title }}
+      <h1 [innerHTML]="title"></h1>
+      <input type="test" [value]="name">
+      {{ name }}<p>One way data flow - changing value in input, does not affect name elsewhere</p> 
     </div>
   `
 })
 export class AppComponent {
   title: string
+  name: string = 'Chris'
   constructor () {
     this.title = 'Ultimate Angular'
   }
